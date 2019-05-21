43a3e5656284fd39aca0bc5cebfb873cc9155c4d
sk-coder <sk954@outlook.com>
2019-05-21 11:36:59 -0400

    Initial Commit of Python Files

---
+++ Python/Loops.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    n = int(raw_input())
+    
+    if (n >= 1 and n <= 20):
+        for i in range(n):
+            print (i**2)
---
+++ Python/Untitled.py
@@ -0,0 +1,10 @@
+n = int(raw_input())
+if n >= 1 && n <= 100:
+    if n % 2 != 0:
+       print "Weird"
+    else if n >= 2 && n < 5:
+        print "Not Weird"
+    else if n >= 6 && n <= 20:
+        print "Weird"
+    else:
+        print "Weird"
