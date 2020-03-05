7824c9d70a08647b936f71185ec3b627b0fcae49
sk-reg <sk954@outlook.com>
2020-03-05 16:13:43 -0500

    Most streamlined procedure so far. Counts most complex sample. Still overruns after an initial mis-match. Needs mechanism for understanding that the end of a swap area has been reached.

--- Python/NewYearChaos2.py
+++ Python/NewYearChaos2.py
@@ -8,60 +8,50 @@ import sys
 
 # Complete the minimumBribes function below.
 def minimumBribes(arr):
-    is_invalid = 0
     arr_len    = len(arr)
-    pos_queue  = 0
-    val_queue  = 0
+    is_invalid = 0
     bribes     = 0
+    in_order   = 1
+    low_idx    = 0
+    last_match = 0
 
     # Iterate through the array to see how many moves needed to be made
     for i in range(arr_len):
-        pos = i + 1
+        idx = i + 1
         val = arr[i]
 
-        if is_invalid:
-            break
+        # A little debug code
+        print "I: " + str(i) + "  Idx: " + str(idx) + "  Val: " + str(val) + "  Ord: " + str(in_order)
 
         # Is the current value greater than the maximum position shift of 2
-        #  this is a fatal flaw
-        if val > pos + 2:
+        if val > idx + 2:
             is_invalid = 1
             break
 
-        # Check the status of our Queues
-        if pos_queue + val_queue == 0:
-            # There is nothing in the queues. Begin checking for numbers that don't match
-            if pos != val:
-                # we found a discontinuity
-                ## save the pos and val into their queues
-                pos_queue = pos
-                val_queue = val
-                ## count the bribe
-                bribes = bribes + 1
+        if in_order:
+            # did we find a mis-match?
+            if idx != val:
+                in_order  = 0
+                print " -- Unordered!"
 
-        # There is an existing discontinuity
         else:
-            if pos == val:
-                # There is an unchanged position in the queue while discord exists
-                # We count this as 2
-                bribes = bribes + 2
-            else:
-                # the numbers don't match. See if it resolves any existing values in the queues
-                if pos_queue == val and val_queue == pos:
-                    # We have resolved this swap. No additional counts. Clear the queues
-                    pos_queue = 0
-                    val_queue = 0
-                elif pos_queue == val:
-                    # We found a match for the position in the value. Increase the count and reset the queue.
-                    pos_queue = pos
+            # is the value less than the index
+            if val < idx:
+                # This person got bribed. Count how many times
+                bribes = bribes + (idx - val)
+
+                if last_match > val:
                     bribes = bribes + 1
-                elif val_queue == pos:
-                    # We found a match for the value in the position
-                    val_queue = val
-                    bribes = bribes + 1
+
+                last_match = val
+            elif val == idx:
+                # This person had to bribe someone to remain in the same spot
+                bribes = bribes + 1
+            print "  Br: " + str(bribes) + "  last: " + str(last_match)
 
 
     # Output the result
+    print "---------------"
     if is_invalid:
         print "Too Chaotic"
     else:
