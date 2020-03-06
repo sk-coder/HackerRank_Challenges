3c2bb447cf9c283b99767c74101bd173f17a6ba4
sk-reg <sk954@outlook.com>
2020-03-06 13:15:27 -0500

    Most streamlined procedure so far. Counts most complex sample.
    Added measure to sum index and position values to determine when
    bribing is over.

--- Python/NewYearChaos2.py
+++ Python/NewYearChaos2.py
@@ -9,53 +9,68 @@ import sys
 # Complete the minimumBribes function below.
 def minimumBribes(arr):
     arr_len    = len(arr)
-    is_invalid = 0
+    is_valid   = 1
     bribes     = 0
     in_order   = 1
-    low_idx    = 0
+    idx_sum    = 0
+    val_sum    = 0
     last_match = 0
 
-    # Iterate through the array to see how many moves needed to be made
-    for i in range(arr_len):
-        idx = i + 1
-        val = arr[i]
+    # Test for a too long array
+    if arr_len >= 10**5:
+        is_valid = 0
 
-        # A little debug code
-        print "I: " + str(i) + "  Idx: " + str(idx) + "  Val: " + str(val) + "  Ord: " + str(in_order)
+    if is_valid:
+        # Continue processing the array
 
-        # Is the current value greater than the maximum position shift of 2
-        if val > idx + 2:
-            is_invalid = 1
-            break
+        # Iterate through the array to see how many moves needed to be made
+        for i in range(arr_len):
+            idx = i + 1
+            val = arr[i]
 
-        if in_order:
-            # did we find a mis-match?
-            if idx != val:
-                in_order  = 0
-                print " -- Unordered!"
+            # Is the current value greater than the maximum position shift of 2
+            if val > idx + 2:
+                is_valid = 0
+                break
 
-        else:
-            # is the value less than the index
-            if val < idx:
-                # This person got bribed. Count how many times
-                bribes = bribes + (idx - val)
+            if in_order:
+                # did we find a mis-match?
+                if idx != val:
+                    in_order = 0
+                    idx_sum  = idx
+                    val_sum  = val
 
-                if last_match > val:
+            else:
+                # add idx and val to sums, we will use this later
+                idx_sum = idx_sum + idx
+                val_sum = val_sum + val
+
+                # is the value less than the index
+                if val < idx:
+                    # This person got bribed. Count how many times
+                    bribes = bribes + (idx - val)
+
+                    if last_match > val:
+                        bribes = bribes + 1
+
+                    last_match = val
+
+                    if idx_sum == val_sum:
+                        # we have found the end of this series of bribes
+                        in_order = 1
+                        idx_sum  = 0
+                        val_sum  = 0
+
+                elif val == idx:
+                    # This person had to bribe someone to remain in the same spot
                     bribes = bribes + 1
-
-                last_match = val
-            elif val == idx:
-                # This person had to bribe someone to remain in the same spot
-                bribes = bribes + 1
-            print "  Br: " + str(bribes) + "  last: " + str(last_match)
 
 
     # Output the result
-    print "---------------"
-    if is_invalid:
-        print "Too Chaotic"
-    else:
+    if is_valid:
         print(bribes)
+    else:
+        print "Too chaotic"
 
 
 if __name__ == '__main__':
